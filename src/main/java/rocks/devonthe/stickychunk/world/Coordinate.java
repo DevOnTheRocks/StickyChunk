ab4417429c422b0b70c543a28ba532d625e3bf9d
Mohron <mohron@mohron.net>
2017-01-31 17:09:22 -0600

    Simplified coordinate class

--- src/main/java/rocks/devonthe/stickychunk/world/Coordinate.java
+++ src/main/java/rocks/devonthe/stickychunk/world/Coordinate.java
@@ -1,31 +1,29 @@
 package rocks.devonthe.stickychunk.world;
 
-import com.flowpowered.math.vector.Vector2i;
 import com.flowpowered.math.vector.Vector3i;
 
 /**
  * Created by Cossacksman on 02/01/2017.
  */
 public class Coordinate {
-	private Vector2i point;
+    private int x;
+    private int z;
 
-	public Coordinate(int x, int z) {
-		this.point = new Vector2i(x, z);
-	}
+    public Coordinate(int x, int z) {
+        this.x = x;
+        this.z = z;
+    }
 
-	public Coordinate(Vector2i point) {
-		this.point = point;
-	}
+    public Coordinate(Vector3i point) {
+        this.x = point.getX();
+        this.z = point.getZ();
+    }
 
-	public Coordinate(Vector3i point) {
-		this.point = new Vector2i(point.getX(), point.getZ());
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getX() {
-		return point.getX();
-	}
-
-	public int getZ() {
-		return point.getY();
-	}
+    public int getZ() {
+        return z;
+    }
 }
